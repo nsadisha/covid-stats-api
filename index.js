d2df281ba8f679305e573e5d1b432224d740c4c4
nsadisha <nsadisha@gmail.com>
2021-07-17 15:43:20 +0530

    2 routes created

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require('express')
 const app = express()
+const request = require('request')
 const ipl = require('ip-locator')
 
-
+// default route
 app.get('/',(req, res, next) => {
   var myip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-
   ipl.getDomainOrIPDetails(myip,'json', function (err, data) {
     var countryCode = data.countryCode
-    res.json({"code": countryCode})
+
+    request('https://api.covid19api.com/total/country/'+countryCode, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var data = JSON.parse(body.toString())
+        res.json(data[data.length-1])
+      }else{
+        res.json(error)
+      }
+    })
+  })
+})
+
+// country code route
+app.get('/:code',(req, res, next) => {
+  var countryCode = req.params.code
+  request('https://api.covid19api.com/total/country/'+countryCode, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body.toString())
+      res.json(data[data.length-1])
+    }else{
+      res.json(error)
+    }
   })
 })
 
