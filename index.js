582ad00ab0bad16b48c0b7632d3ac6f7e3f4c48c
nsadisha <nsadisha@gmail.com>
2021-07-17 17:15:24 +0530

    day by day stats implemented

--- index.js
+++ index.js
@@ -32,6 +32,36 @@ app.get('/:code',(req, res, next) => {
     }
   })
 })
+// day by day
+app.get('/days/:code',(req, res, next) => {
+  var countryCode = req.params.code
+  var isCumulative = req.query.cumulative
+  request('https://api.covid19api.com/total/dayone/country/'+countryCode, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body.toString())
+
+      // processing data
+      if(isCumulative=="false"){
+        let confirmedCount = data[0].Confirmed
+        let deathCount = data[0].Deaths
+        let recoveredCount = data[0].Recovered
+        for(let i=1; i<data.length; i++){
+          data[i].Confirmed -= confirmedCount
+          confirmedCount += data[i].Confirmed
+          data[i].Deaths -= deathCount
+          deathCount += data[i].Deaths
+          data[i].Recovered -= recoveredCount
+          recoveredCount += data[i].Recovered
+        }
+      }
+
+      res.json(data)
+    }else{
+      res.json(error)
+    }
+  })
+})
+
 
 app.listen(process.env.PORT || 3000, ()=>{
   console.log("listening for requests")
